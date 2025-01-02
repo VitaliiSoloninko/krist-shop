96875c6d1f586155a004f30494a19ad19b2cd00b
22051988Di <vitalii.soloninko@gmail.com>
2025-01-02 15:14:52 +0100

    0.4	Header standard layout

--- src/app/widgets/header/header.component.ts
+++ src/app/widgets/header/header.component.ts
@@ -1,11 +1,36 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  openMenu = false;
 
+  menuItems = [
+    {
+      name: 'Shop',
+      link: 'shop',
+    },
+    {
+      name: 'Product',
+      link: 'product',
+    },
+    {
+      name: 'Auth',
+      link: 'auth',
+    },
+    {
+      name: 'Admin',
+      link: 'admin',
+    },
+    {
+      name: 'Basket',
+      link: 'basket',
+    },
+  ];
 }
